dd931a0a8ad91ccc73fc9901ab5a4bc175da4b0f
Pratikkumar Mohite <mohite770.pm@gmail.com>
2024-08-08 14:47:13 +0530

    creating aws connection using env variables

--- pkg/aws/connection.go
+++ pkg/aws/connection.go
@@ -1,5 +1,16 @@
 package aws
 
-func Connection() string {
-	return "Connected to AWS"
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+func AWSConnection(profile string) aws.Config {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(profile))
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+	return cfg
 }
