8840ef37a28d1782d4db1e7fdd20c86c8c0b004b
Pratikkumar Mohite <mohite770.pm@gmail.com>
2024-08-08 14:47:59 +0530

    get env AWS_PROFILE

--- cmd/cleanup.go
+++ cmd/cleanup.go
@@ -1,5 +1,11 @@
 package cmd
 
-func Cleanup() string {
-	return "Cleanup complete"
+import (
+	"github.com/pratikkumar-mohite/s3-cleanup/pkg/aws"
+)
+
+func S3Cleanup() {
+	config := aws.AWSConnection(getFromEnv("AWS_PROFILE"))
+	s3Client := aws.S3Connection(config)
+	aws.ListS3Buckets(s3Client)
 }
--- cmd/utils.go
+++ cmd/utils.go
@@ -1,5 +1,14 @@
 package cmd
 
-func UtilsCleanup() string {
-	return "Cleanup complete"
+import (
+	"fmt"
+	"os"
+)
+
+func getFromEnv(env string) string {
+	home := os.Getenv(env)
+	if home == "" {
+		panic(fmt.Sprintf("Environment variable %s is not set", env))
+	}
+	return home
 }
