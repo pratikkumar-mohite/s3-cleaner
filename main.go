b502391281161dbdac48181ab16e9002679a6d02
Pratikkumar Mohite <mohite770.pm@gmail.com>
2024-08-20 20:23:13 +0530

    upload dummyfiles to s3 for testing

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/pratikkumar-mohite/s3-cleanup/cmd/s3-cleanup"
 )
 
 func main() {
-	startTime := time.Now()
 	cmd.S3Cleanup()
-	elapsedTime := time.Since(startTime)
-	fmt.Println("Total time taken: ", elapsedTime)
 }
